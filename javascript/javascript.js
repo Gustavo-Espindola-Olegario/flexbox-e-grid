1a61018e7f287823a7247c8a38eb355b23c02a12
Gustavo-Espindola-Olegario <gustavo_espindola1999@hotmail.com>
2022-05-03 18:45:50 -0300

    site responsivo com GridLayout e FlexBox

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+let mostrar = true;
+
+const menu = document.querySelector(".menu");
+const menuSecreto = menu.querySelector(".menu-secreto");
+
+menuSecreto.addEventListener("click", () => {
+    document.body.style.overflow = mostrar ? "hidden" : "initial";
+    menu.classList.toggle("on", mostrar);
+    mostrar = !mostrar;
+})
